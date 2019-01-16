06cf5a51985048a768810e9e0c75637e1ec12e5c
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-16 00:41:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  ii=0 
+  size= array.size
+  newarr = []
+  if size>2
+    while (ii<array.size-1)
+     ele = array[ii]
+      array[ii] = ele + ","
+      ii+=1
+    end
+  end  
+  if size > 1
+    array.insert(-2 , "and")
+  end 
+  sent = array.join(" ")
+  return sent
 end
